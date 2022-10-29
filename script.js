d8981ec9360b46f41abf24c7708a378bab2da7bf
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-10-29 18:05:48 +0300

    complete hw

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
 // порахувати, скільки секунд у цій кількості годин;
 // записати обчислене значення у змінну;
 // вивести цю змінну користувачеві через alert.
+
+
+ hoursN = prompt("Enter the number of hours for recalculation:");
+
+ secondsN = hoursN * 3600;
+
+ alert(`${hoursN} hour(s) contains ${secondsN} seconds`);
