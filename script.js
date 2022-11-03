7ae681d1df1c9e831435945b2dc8e82043b37f9d
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-03 10:50:43 +0200

    add finished hw

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 // отримати від користувача через три prompt три числа
 // показати через alert середнє арифметичне цих чисел
+
+
+number1 = +prompt("Enter firs Number");
+number2 = +prompt("Enter second Number");
+number3 = +prompt("Enter third Number");
+
+averageN = (number1 + number2 + number3)/3;
+
+alert( `Arithmetic mean = ${averageN}`);
