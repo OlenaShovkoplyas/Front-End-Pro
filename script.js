294f0ae5e06ab1ad83d26bcf680ca1120477ca39
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-03 11:33:11 +0200

    add changes

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 // показати через alert середнє арифметичне цих чисел
 
 
-number1 = +prompt("Enter firs Number");
-number2 = +prompt("Enter second Number");
-number3 = +prompt("Enter third Number");
+number1 = parseInt(+prompt("Enter firs Number"));
+number2 = parseInt(+prompt("Enter second Number"));
+number3 = parseInt(+prompt("Enter third Number"));
 
-averageN = parseInt((number1 + number2 + number3)/3);
+averageN = (number1 + number2 + number3)/3;
 
 alert( `Arithmetic mean = ${averageN}`);
 
