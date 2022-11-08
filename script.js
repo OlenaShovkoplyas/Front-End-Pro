42b5fed52be1ee021f16d04c9c5877dd404b8511
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-08 15:01:54 +0200

    finaly!! Fin

--- script.js
+++ script.js
@@ -9,4 +9,93 @@
 
 // 4) С помощью alert или console.log выводим финальный результат действия (+ - / *).
 
-action = prompt("Enter action:");
+
+
+do { 
+    getAction = prompt("Enter mathematical action you need (+ - / *):");
+    correctAction = getAction === "+" || getAction === "-" || getAction === "*" || getAction === "/";
+    if (!correctAction) {
+         alert("Entered action has wrong format");
+    }
+} while (!correctAction);
+
+do { 
+    getOperands = parseInt(prompt("Enter number of operands you need (from 1 to 7):"));
+    operandsNumber = getOperands >= 1 && getOperands <= 7;
+    if (!operandsNumber) {
+         alert("Entered wrong amount of operands");
+    }
+} while (!operandsNumber);
+
+result = 0;
+
+switch (getAction) {
+    case "+":
+        for (i = 1; i <= getOperands; i++) {
+            do {
+                number = parseInt(prompt(`Emtern ${i} numer for operation`));
+            } while (isNaN(number));
+
+            result += number;
+        }
+    break;  
+
+    case "-":
+        for (i = 1; i <= getOperands; i++) {
+            do {
+                number = parseInt(prompt(`Emtern ${i} numer for operation`));
+            } while (isNaN(number));
+            if (i === 1) {
+                result = number;
+            } else {
+                result -= number;
+            }
+        }
+    break;  
+    
+    case "*":
+        result = 1;
+
+        for (i = 1; i <= getOperands; i++) {
+            do {
+                number = parseInt(prompt(`Emtern ${i} numer for operation`))
+            }
+            while (isNaN(number));
+
+            result *= number;
+        }
+        break;
+
+    case "/":
+        for (i = 1; i <= getOperands; i++) {
+            do {
+                number = parseInt(prompt(`Emtern ${i} numer for operation`))
+
+                if (number === 0) {
+                    alert("Error! You can't divide by 0!!!")
+                }
+            }
+            while (number === 0 || isNaN(number));
+
+            if (i === 1) {
+                result = number;
+            }
+            else {
+                result /= number;
+            }
+        }
+
+        break;
+
+}
+
+alert(`Result of calculation is: ${result}`);
+
+
+
+
+
+
+    
+
+   
