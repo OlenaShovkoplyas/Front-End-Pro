0cb3d5d64aa32eec264ac534ba939e9f5462f40d
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-08 16:02:31 +0200

    not fix

--- script.js
+++ script.js
@@ -2,3 +2,34 @@
 // Запустить цикл перебора от a до b с шагом h (ввод с клавиатуры). 
 // Посчитать сумму факториалов* чисел которые попадаются во время перебора.
 
+
+do {
+    firstNumber = parseInt(prompt("Enter your first number"));
+    secondNumber = parseInt(prompt("Enter your second number"));
+    correctNumbers = firstNumber >=0 && secondNumber > firstNumber
+   
+    if (!correctNumbers) {
+        alert (`Enter correct number! First number must be >=0, and the second must be more, then the first one`);
+    }
+   } while (!correctNumbers);
+
+   do {
+    step = parseInt(prompt("Enter the step for numbers"));
+    correctStep = step >0 && step < (secondNumber - firstNumber)
+
+    if (!correctStep) {
+        alert (`Note, that step must bo more, than 0, but less than difference between numbers`);
+    }
+   } while (!correctStep);
+
+
+   for(firstNumber; firstNumber <= secondNumber; firstNumber += step) {
+        factorial = 1;
+        
+        for(j = firstNumber; j >= 1; j++) {
+            factorial *= j;
+        }
+        sum += factorial;
+    }
+
+alert (`Sum of factorials is ${sum}`);
