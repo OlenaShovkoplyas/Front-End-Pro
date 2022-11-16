f93ee35d3628d3f655bb16ce9e7bdb20fb0f389e
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-16 12:24:03 +0200

    fin hw

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 // Створити масив, довжину та елементи якого задає користувач.
 
+arr = [];
+
+do {
+    arrLenth = parseInt(prompt(`Enter the lenth of your array`));
+} while (!(arrLenth > 0));
+
+for (i = 0; i < arrLenth; i++) {
+    do {
+        arrValue = parseInt(prompt(`Enter ${arrLenth} values for array`));
+    } while (isNaN(arrValue));
+
+    arr[i] = arrValue;
+} 
+alert(`Your array is ${arr}`);
+
 // Відсортувати масив за зростанням.
 
-// Видалити елементи з масиву з 2 по 4 (включно!).
+for (i = 0; i < arrLenth; i++) {
+    swap = false;
 
-// У міру змін виводити вміст масиву на сторінку.
+    for (j = i + 1; j < arrLenth; j++) {
+        if (arr[i] > arr[j]) {
+            swap = arr[j];
+            arr[j] = arr[i];
+            arr[i] = swap;
+        }
+    }
+}
+alert(`Sorted array is ${arr}`);
+
+// Видалити елементи з масиву з 2 по 4 (включно!). У міру змін виводити вміст масиву на сторінку.
+
+if (arrLenth >= 5) {
+    arr.splice(1, 3);
+    alert(`Array without elements from second to forth is ${arr}`)
+}
+
+    
+
+
