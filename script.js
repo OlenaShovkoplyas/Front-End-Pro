efbfe7d804e6a2c19b50d5727a172587874f3800
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-18 21:53:39 +0200

    fin

--- script.js
+++ script.js
@@ -8,33 +8,39 @@
 hero = ['Ivan'];
 native = ['York','Of'];
 destination = ['Poltava','In'];
-rainbow = [];
-rainbow.push(hero, native, destination);
+
+rainbow = hero.concat(native, destination);
+// document.write(`${rainbow}`);
 
 // 2. Сделать реверс элементов полученного массива rainbow.
 
-for (i = 0; i < rainbow.length; i++) {
-    (j = 1; j <= i; j++)
-    document.write(`${rainbow}`);}
-
-  
+rainbowReversed = rainbow.reverse(); //In, Poltava, Of, York, Ivan
     
-
-
 // 3. Изменить/добавить элементы массива rainbow так, что бы в итоге получился массив вида ['Richard','Of','York','Gave','Battle','In','Vain'];
+
+rainbowReversed.splice(0, 2); //delate 'In', 'Poltava'
+
+rainbowReversed.unshift('Richard'); // add 'Richard' on the begining
+
+rainbowReversed.pop();  // cut last 'Ivan'
+
+rainbowReversed.push('Gave','Battle','In','Vain');  //add at the end 'Gave','Battle','In','Vain'
+
 // 4. Вывести элементы полученного массива в html в виде, который представлен в прикрепленном рисунке rainbow.png.
 // Шрифт надписей на картинке Quicksand. Вы можете взять любой))
-// Использовать все методы не обязательно! 
-// Берем только те, которые помогут вам привести массив к такому виду, что бы вывести его как на прикрепленной картинке rainbow.png.
 
-// rainbowCircles = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'violet'];
 
-// for(i = 0; i < rainbowCircles.length; i++) {
-//      document.write(`
-//         <div class="wrap">
-//              <div class="circle" style="background-color: ${rainbowCircles[i]}"></div>
-//          </div>
-//      `)
-//  };
+rainbowCircles = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'violet'];
+
+for(i = 0; i < rainbowCircles.length; i++) {
+   
+     document.write(`
+        <div class="wrap">
+             <div class="circle" style="background-color: ${rainbowCircles[i]}"></div>
+             <div class="text">${rainbowReversed[i]}</div>
+         </div>        
+    `)
+ };
+
 
  
