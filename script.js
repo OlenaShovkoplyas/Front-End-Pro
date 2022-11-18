4982ccda2ced176cc6e46da907c180314bfac19b
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-18 20:16:49 +0200

    +

--- script.js
+++ script.js
@@ -3,10 +3,38 @@
 // native = ['York','Of'];
 // destination = ['Poltava','In'];
 // Используя циклы, if/else, switch/case и методы .push(), .pop(), .unshift(), .shift(), .concat(), .splice(), .reverse() .join():
-// 1. Объединить массивы destination, native, hero в общий массив rainbow.
+// 1. Объединить массивы destination, native, hero в общий массив rainbow.\
+
+hero = ['Ivan'];
+native = ['York','Of'];
+destination = ['Poltava','In'];
+rainbow = [];
+rainbow.push(hero, native, destination);
+
 // 2. Сделать реверс элементов полученного массива rainbow.
+
+for (i = 0; i < rainbow.length; i++) {
+    (j = 1; j <= i; j++)
+    document.write(`${rainbow}`);}
+
+  
+    
+
+
 // 3. Изменить/добавить элементы массива rainbow так, что бы в итоге получился массив вида ['Richard','Of','York','Gave','Battle','In','Vain'];
 // 4. Вывести элементы полученного массива в html в виде, который представлен в прикрепленном рисунке rainbow.png.
 // Шрифт надписей на картинке Quicksand. Вы можете взять любой))
 // Использовать все методы не обязательно! 
 // Берем только те, которые помогут вам привести массив к такому виду, что бы вывести его как на прикрепленной картинке rainbow.png.
+
+// rainbowCircles = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'violet'];
+
+// for(i = 0; i < rainbowCircles.length; i++) {
+//      document.write(`
+//         <div class="wrap">
+//              <div class="circle" style="background-color: ${rainbowCircles[i]}"></div>
+//          </div>
+//      `)
+//  };
+
+ 
