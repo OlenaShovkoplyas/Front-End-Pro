4a851ebb04a79208a63ee5951bdb29e749cf76ae
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-29 12:35:39 +0200

    +

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 // sum(3) = 3
 // sum(5) = 8
 // sum(20) = 28
+
+var getAdder = function () { var res = 0; return function(x) { res += x; return res; } };
+var sum = getAdder();
+sum(1); // 1
+sum(3); // 4
+var sum2 = getAdder(); // два независимых сумматора
+sum2(1); //1
+sum2(3); // 4
