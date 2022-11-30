28640cba8cfe636fc9593d99907afb3b972fb6b5
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-11-30 11:06:21 +0200

    +

--- script.js
+++ script.js
@@ -4,26 +4,28 @@
 
 // newList = copy(list, function(value){ return value*10; })
 
-// let a = 8
-// function value (a) { 
-//     return a * 10;
-// } 
 
-arr = [[1,2,3,4], 45];
 
-function cloneArrayFunction(list, func){
-    const copyArr = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(typeof function () {} === "function"){
-        copyArr.push(i);
-        cloneArrayFunction(i);
-    } if(Array.isArray(arr[i])) {
-        copyArr.push(arr[i].slice());
-    } else {
-        copyArr.push(arr[i]);
-        return copyArr;   
-        cloneArrayFunction =  copyArr;
-    } 
-  }
-  console.log(copyArr);
-}
+const list = [1, 3, 8, [1,2,3,4], 45, 'a'];
+
+function copy(list, func){
+    const newList = [];
+    for(let i = 0; i < list.length; i++) {
+        if(typeof(func) === 'function'){
+            func(list[i]);
+        }
+        newList.push(list[i]);
+        }
+        return newList;
+    }
+
+    function copyArrayFunction(value) {
+        console.log(value);
+    }
+    let a = 8
+    
+  
+ newList = copy(list, function (value) { return value * 10; }); 
+ a = copy(list, copyArrayFunction);
+ b = copy(newList);
+ c = copy([2, 3, 4]);
