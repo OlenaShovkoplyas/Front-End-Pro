39f06901a9fab2b4e3583cba86eed23004ddf2a9
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-12-07 23:55:48 +0200

    +

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Для элементов массивов monday и tuesday необходимо выполнить следующие методы:
-// Оставить только те задачи, на выполнение которых нужно более 2-х часов.
-// Умножить результат на часовую ставку (amount) и добавить полученное значение в качестве третъего элемента в массив.
-
 // Используем только методы concat/forEach/map/filter/join (по надобности, все не нужно).
-
 let amount = 100;
 let monday = [
   ['Write a tutorial',180],
@@ -15,11 +10,41 @@ let tuesday = [
    ['A whole lot of nothing',240]
 ];
 
-// Сконвертировать время потраченное на выполнение задач в часы, вместо минут.
+const newArr = monday.concat(tuesday)
 
-// Вывести в html таблицу, которая состоит из ячеек с задачами в виде:
-// <tr>
-//   <td>Task name: Write a tutorial</td>
-//   <td>Taks duration: 3 hours</td>
-//   <td>Task amount: $300</td>
-// </tr>
+// console.log(newArr);
+
+.map(function(task) {     // Сконвертировать время потраченное на выполнение задач в часы, вместо минут.
+  task[1] = task[1]/60;    
+  return task; 
+})
+
+.filter(function(task) {       // Оставить только те задачи, на выполнение которых нужно более 2-х часов.
+  task[1] > 2;
+  return task;
+  })
+  
+.map(function(task) {            // Умножить результат на часовую ставку (amount) и добавить полученное значение в качестве третъего элемента в массив.
+  task.push(task[1] * amount);
+  return task;
+  })
+  
+.map(function(task){
+  return
+    `<tr>
+      <td>Task name: ${task[0]}</td>
+      <td>Taks duration: ${task[1]} hours</td>
+      <td>Task amount: ${task[2]}$</td>
+    </tr>`
+  })
+  .join(" ");
+    
+
+
+document.write(`
+<table>
+<tbody>${newArr}</tbody>
+</table>`)
+
+
+
