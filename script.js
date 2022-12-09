d385ef0dad138d861eb5cc5f86c0178778869e14
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-12-09 23:50:15 +0200

    need to be improved(shows 10 instead 20)

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const obj = {
         p: 13
     }
 };
-
 // Написать функцию convert(obj), которая получает аргументом obj. 
 //Функция должна вернуть новый объект вида:
 // newObj = {
@@ -22,10 +21,37 @@ const obj = {
     //     k: 23,
     //     p: 13
     // }
+    
+    
+// function convert(obj) {
+//     const newObj = {};
+//     getProp(obj);
+//         function getProp(obj) {
+//         for(let key in obj) {
+//             if(typeof(obj[key]) === 'object') {
+//                 getProp(obj[key]);
+//             } else {
+//                 console.log(obj[key]);                              
+//             }            
+//         }        
+//     }     
+//     newObj = newObj(getProp(obj)); 
+//     return  newObj;    
+// }
+// convert(obj);   
+// console.log(newObj);
 
-//    function convert(obj){
-//     for (let key in obj){
+function convert(object) {
+    for (let key in obj){
+        // console.log(obj[key])
+        if (typeof obj[key] === 'object'){
+            copied = obj[key];
+            // console.log(copied)
+            delete obj[key];
+        }   
+    }   
+    return Object.assign(obj, copied); 
+} 
 
-//     }
-//    }
-
+const newObj = convert(obj);   
+console.log(newObj);
