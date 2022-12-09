901e4be9ee672a6758178c2a833521f588b3add6
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-12-09 21:46:33 +0200

    --

--- script.js
+++ script.js
@@ -4,29 +4,3 @@
 // assignObjects({ x: 10, y: 20 }, { z: 30 }) вывод { x:10, y:20, z: 30 }
 // assignObjects({ x: 10 }, { x: 20, y: 30 }) вывод { x:20, y: 30 }
 
-let obj1 = {
-    x: 10,
-    y: 20
-},  
-    obj2 = {
-    z: 30  
-}
-
-function assignObjects() {
-    let args = arguments,
-        resobj = {},
-        obj1 = args[0],
-        obj2 = args[1],
-        last = args[args.length - 1]       
-    for (let key in obj1) {
-        resobj[key] = obj1[key];
-    }
-    for (let key in obj2) {
-        if (flag && resobj[key]) {
-            continue;
-        }
-        resobj[key] = obj2[key];
-    }
-    return resobj;
-    console.log(resobj);
-}
