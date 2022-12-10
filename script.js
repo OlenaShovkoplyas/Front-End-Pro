d64d378be0e8a6396d012a160a3539dce70a54ff
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-12-10 07:50:52 +0200

    idn

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Создать объект с такой структурой: 
+// Написать функцию convert(obj), которая получает аргументом obj. 
+//Функция должна вернуть новый объект вида:
+// newObj = {
+    //     x: 20,
+    //     y: 20,
+    //     z: 30,
+    //     k: 23,
+    //     p: 13
+    // }
 
 const obj = {
     x: 10, 
@@ -12,60 +21,37 @@ const obj = {
         p: 13
     }
 };
-// Написать функцию convert(obj), которая получает аргументом obj. 
-//Функция должна вернуть новый объект вида:
-// newObj = {
-    //     x: 20,
-    //     y: 20,
-    //     z: 30,
-    //     k: 23,
-    //     p: 13
-    // }
-    
 
 // function convert(obj) {   
 //     getProp(obj);
 //         function getProp(obj) {
 //         for(let key in obj) {
 //             if(typeof(obj[key]) === 'object') {
-//                 getProp(obj[key]);
-//             } else {
-//                 console.log(obj[key]);                              
-//             }            
-//         }        
+//                 copy = getProp(obj[key]),
+//                 delete obj[key];                                        
+//             }   
+//             return obj[key];                     
+//         }     
+//         return Object.assign(obj, copy);           
 //     }     
-//     // return  Object.assign(obj);    
+   
 // }
-convert(obj);   
-console.log(obj);
-
-function convert(obj) {
-     
-    for(let key in obj) {
-        if(typeof obj[key] === 'object') {
-            convert(obj[key]);
-        } else {
-            console.log(obj[key]);
-        }
-    }
-
-}
-convert(obj);   
-console.log(obj);
-
-
-
-// function convert(object) {
-//     for (let key in obj){
-//         // console.log(obj[key])
-//         if (typeof obj[key] === 'object'){
-//             copied = obj[key];
-//             // console.log(copied)
-//             delete obj[key];
-//         }   
-//     }   
-//     return Object.assign(obj, copied); 
-// } 
 
 // const newObj = convert(obj);   
 // console.log(newObj);
+
+function convert(object) {
+    for (let key in obj){
+        if (typeof obj[key] === 'object'){
+            copied = obj[key];            
+            return copied;
+        }
+        // } else {
+        //     return obj[key];
+        // }
+    }   
+    return Object.assign(obj, copied); 
+} 
+
+const newObj = convert(obj);   
+console.log(newObj);
