2e2219c021e37c568c2b1ea2d10f149a30c393d7
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2022-12-12 11:18:14 +0200

    add vers/2

--- script.js
+++ script.js
@@ -37,3 +37,22 @@ function convert(obj) {
         return newObj;                   
     } 
 console.log(convert(obj));
+
+
+//2й вариант записи через assign
+
+// function convert(object) {
+//     const newObj = {};
+
+//     for(let key in object) {
+//         if(typeof object[key] === "object" && !Array.isArray(object[key])) {
+//             Object.assign(newObj, convert(object[key]));
+//         } else {
+//             newObj[key] = object[key];
+//         }
+//     }
+
+//     return newObj;
+// }
+
+// console.log(convert(obj));
