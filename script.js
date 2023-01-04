85b2081deb5d11ce4a157f98bc3be996140432a0
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2023-01-04 17:34:56 +0200

    add fin

--- script.js
+++ script.js
@@ -2,42 +2,56 @@
 // Додати до екземпляра метод - check(obj), параметр obj якого має властивості X, Y, znak. 
 // Метод повинен підтвердити у користувача, чи хоче він зробити дію znak c Х і У. 
 // Якщо хоче, зробити математичну дію znak (яка описана в прототипі), інакше - запитати введення нових даних через метод input() класу SuperMath.
-
 // Приклад об'єкта: `obj = {X:12, Y:3, znak: “/”}`, можливі варіанти znak => `+ - / * %`.
-
 // При введенні znak потрібно перевірити коректність введення на можливі математичні дії.
-
 // p = new SuperMath();
 // p.check(obj); // --> no p.input() -> 3 prompt -> рахує
 
 
+
+
+
+const OPERATIONS = {
+	"+": function(x, y) { return x + y },
+	"-": function(x, y) { return x - y },
+	"*": function(x, y) { return x * y },
+	"/": function(x, y) { return x / y },
+};
+
 class SuperMath {
-    constructor(mark, model, kuzov)
+	constructor() {
+		this.input();
+	}
+
+   input() {
+		do {
+			this.x = +prompt(`Enter x`)
+		} while(isNaN(this.x));
+
+		do {
+			this.y = +prompt(`Enter y`)
+		} while(isNaN(this.y));
+
+		do {
+			this.znak = prompt(`Enter operands: ${this.getOperands()}`)
+		} while(!OPERATIONS[this.znak]);
+	}
+
+    getOperands() {
+		let operands = [];
+		for(let key in OPERATIONS) {
+			operands.push(key);
+		}
+
+		return operands.join(", ")
+	}
+
+	check() {
+		let userApproved = confirm(`Do you want make operation ${this.x} ${this.znak} ${this.y}?`);
+
+		return userApproved ? OPERATIONS[this.znak](this.x, this.y) : this.input().check();
+	}	
 }
+const obj = new SuperMath();
 
-
-
-// class Car {
-// 	constructor(mark, model, kuzov) {
-// 		this.mark = mark;
-// 		this.model = model;
-// 		this.kuzov = kuzov;
-// 	}
-
-// 	render() {
-// 		let list = [];
-	
-// 		for(let key in this) {
-// 			list.push(`<li>${key}: ${this[key]}</li>`);
-// 		}
-	
-// 		return list.join("");
-// 	}
-// }
-
-// const BMW = new Car("BMW", "X5", "universal");
-// const Audi = new Car("Audi", "S5", "sedan", 350);
-
-
-// console.log(BMW);
-// console.log(Audi);
+alert(`Result is ${obj.check()}`);
